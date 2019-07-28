62213494d4d658efec7a221bf48d30e4f2131cd8
zhoushoujian <863165094@qq.com>
2019-07-28 23:14:05 +0800

    add client close check

--- src/server.js
+++ src/server.js
@@ -258,7 +258,8 @@ wss.on('connection', function connection(ws, req) {
 	});
 	ws.on('close', (code, msg) => {
 		if(code === 1001){
-			writeWSResponse('当前共' + wss.clients.size + '位游客', 'order-string')
+			writeWSResponse('当前共' + wss.clients.size + '位游客', 'order-string');
+			ws.terminate()
 		}
 	})
 });
